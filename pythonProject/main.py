414230136105c5e8bd2f0458406069e0db81c82f
Jessicamelorodrigues <108497533+Jessicamelorodrigues@users.noreply.github.com>
2022-06-30 21:43:06 -0300

    Aula 01 de Phyton
    Aprendemos sobre manipulação de texto e variáveis.

---
+++ pythonProject/main.py
@@ -0,0 +1,27 @@
+a="ana maria joana" #criando uma variável.
+"""
+Bloco de notas
+"""
+print(a) #exibe
+
+print(a.upper()) # deixa tudo maiúsculo
+
+print(a.lower()) #deixa tudo minusculo
+
+print(a.capitalize()) #deixa as primeiras maiusculas
+
+print(a.title()) #deixa somente a primeira em maiúsculo
+b= a.split(" ",1)
+print(b)
+
+variavel02=" casa"
+
+variavel03=variavel02.replace("sa","sinha") #troca uma letra por outra letra
+print(variavel03)
+print(variavel02)
+
+print(variavel02.find("a")) #localiza onde esta uma palavra ou letra dentro de uma variavel
+
+print(len(variavel02))#conta o número de caracteres a partir de 0
+print(variavel03.strip()) #tira o espaço no inicio e no final das palavras
+
